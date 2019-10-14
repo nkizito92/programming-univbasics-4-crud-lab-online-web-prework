6f2fff0b80f845e4cb78f7d27fb283f9953d0b8e
Kizito Njoku <nkizito57@gmail.com>
2019-10-14 03:58:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,15 +24,15 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
- p array[index_number]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
- p array[0];
+  array[0];
 end
 
 def retrieve_last_element_from_array(array)
- p array[-1]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
