8162f59256b3541f103d6e6441041dc1f6d9df53
Kizito Njoku <nkizito57@gmail.com>
2019-10-14 03:54:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["hello", "hi", "whats up"]
+  array = ["hello", "hi", "whats up", "Sup"]
 end
 
 def add_element_to_end_of_array(array, element)
