6456b05b3258185e50e91f49dff658904084a654
Kizito Njoku <nkizito57@gmail.com>
2019-10-14 03:51:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,34 @@
 def create_an_empty_array
-  
+  array = []
 end
 
 def create_an_array
-  
+  arr = ["hello", "hi", "whats up"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["hello", "hi", "whats up"]
+  array.push(element);
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["hello", "hi", "whats up"]
+  array.unshift(element);
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["hello", "hi", "whats up"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["hello", "hi", "whats up"]
+  arr.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["hello", "hi", "whats up"]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
